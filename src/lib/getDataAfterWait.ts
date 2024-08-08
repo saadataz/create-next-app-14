0c61c309aa571b259aea1a6896918cb071d9e116
Saadat Azim <saazim@amazon.com>
2024-08-08 21:48:49 +0000

    added 25-index-routes

--- src/lib/getDataAfterWait.ts
+++
@@ -1,7 +0,0 @@
-export async function getDataAfterWait(waitTimeMs: number) {
-  const startTime = Date.now();
-  await new Promise((res) => setTimeout(res, waitTimeMs));
-  const duration = Date.now() - startTime;
-
-  return { startTime, duration };
-}
